75dc1e158e3356cfe37acb1dab5298e2702f3b6b
PoeticBliss <meremezeukah@gmail.com>
2016-05-31 23:29:18 -0400

    export check function

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-exports.psquare = {
-  // This will return true if a number is a perfect square
-  check: function(num) {
-    // convert input to integer
-    num = parseInt(num);
-    // get the square root of the number
-    var x = parseInt(Math.sqrt(num));
-    // return true or false
-    return ((x * x) === num);
-  }
+exports.check = function (num) {
+  // convert input to integer
+  num = parseInt(num);
+  // get the square root of the number
+  var x = parseInt(Math.sqrt(num));
+  // return true or false
+  return ((x * x) === num);
 }
